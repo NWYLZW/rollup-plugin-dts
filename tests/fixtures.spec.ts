8f2b4809d435fe5830988570b7960a69420f142d
YiJie <yijie4188@gmail.com>
2024-11-23 21:20:30 +0800

    refactor: rename test case title

--- tests/fixtures.spec.ts
+++ tests/fixtures.spec.ts
@@ -4,27 +4,32 @@ import { resolve } from "node:path";
 import { expect, test, describe } from "vitest";
 import fs from "node:fs";
 
+function build(path: string) {
+  const resolveByPkg = (...paths: string[]) => resolve(process.cwd(), `./fixtures/${path}`, ...paths);
+  if (fs.existsSync(resolveByPkg('node_modules')))
+    fs.rmdirSync(resolveByPkg('node_modules'), { recursive: true });
+
+  execSync('npm i', {
+    cwd: resolveByPkg(),
+    stdio: 'inherit'
+  });
+  execSync('npm run build', {
+    cwd: resolveByPkg(),
+    stdio: 'inherit'
+  });
+
+  fs.rmdirSync(resolveByPkg('./node_modules'), { recursive: true });
+  const files = fs.readdirSync(resolveByPkg('./dist'), { recursive: true });
+  expect(files).toMatchSnapshot();
+  files.forEach(file => {
+    if (typeof file !== 'string') return;
+    if (fs.statSync(resolveByPkg('./dist', file)).isDirectory()) return;
+
+    expect(fs.readFileSync(resolveByPkg('./dist', file), 'utf-8')).toMatchSnapshot();
+  });
+  fs.rmdirSync(resolveByPkg('./dist'), { recursive: true });
+}
+
 describe('fixtures', () => {
-  test('project-with-references', () => {
-    const resolveByPkg = (...paths: string[]) => resolve(process.cwd(), './fixtures/project-with-references', ...paths);
-    if (fs.existsSync(resolveByPkg('node_modules')))
-      fs.rmdirSync(resolveByPkg('node_modules'), { recursive: true });
-
-    execSync('npm i', {
-      cwd: resolveByPkg(),
-      stdio: 'inherit'
-    });
-    execSync('npm run build', {
-      cwd: resolveByPkg(),
-      stdio: 'inherit'
-    });
-
-    fs.rmdirSync(resolveByPkg('./node_modules'), { recursive: true });
-    const files = fs.readdirSync(resolveByPkg('./dist'));
-    expect(files).toMatchSnapshot();
-    files.forEach(file => {
-      expect(fs.readFileSync(resolveByPkg('./dist', file), 'utf-8')).toMatchSnapshot();
-    });
-    fs.rmdirSync(resolveByPkg('./dist'), { recursive: true });
-  })
+  test('project with references', build.bind(null, 'project-with-references'));
 })
